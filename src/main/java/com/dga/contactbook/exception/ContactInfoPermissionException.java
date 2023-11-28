fe08ba8276b9794a14e78ae0e5afc78f548fd895
temo <chxaidzetemo@gmail.com>
2023-11-28 15:59:41 +0400

    clean up

--- src/main/java/com/dga/contactbook/exception/ContactInfoPermissionException.java
+++ src/main/java/com/dga/contactbook/exception/ContactInfoPermissionException.java
@@ -2,6 +2,6 @@ package com.dga.contactbook.exception;
 
 public class ContactInfoPermissionException extends RuntimeException{
     public ContactInfoPermissionException(String actionType,Long id) {
-        super("You are not authorized to"+actionType+ "this contact info. With id: " + id);
+        super("You are not authorized to "+actionType+ " this contact info. With id: " + id);
     }
 }
